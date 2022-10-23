e55337feff89f4e689ac22c3bff1acf51a408b48
COSMIN-NICOLAE CUCU <cosmin.cucu@stud.ubbcluj.ro>
2022-10-23 17:44:03 +0300

    Add project files.

---
+++ Cucu_Cosmin_Lab2/Data/Cucu_Cosmin_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Cucu_Cosmin_Lab2.Models;
+
+namespace Cucu_Cosmin_Lab2.Data
+{
+    public class Cucu_Cosmin_Lab2Context : DbContext
+    {
+        public Cucu_Cosmin_Lab2Context (DbContextOptions<Cucu_Cosmin_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cucu_Cosmin_Lab2.Models.Book> Book { get; set; }
+
+        public DbSet<Cucu_Cosmin_Lab2.Models.Publisher> Publisher { get; set; }
+    }
+}
---
+++ Cucu_Cosmin_Lab2/Pages/Books/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Cucu_Cosmin_Lab2.Data;
+using Cucu_Cosmin_Lab2.Models;
+
+namespace Cucu_Cosmin_Lab2.Pages.Books
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Cucu_Cosmin_Lab2.Data.Cucu_Cosmin_Lab2Context _context;
+
+        public DetailsModel(Cucu_Cosmin_Lab2.Data.Cucu_Cosmin_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public Book Book { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Book = await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Book == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
